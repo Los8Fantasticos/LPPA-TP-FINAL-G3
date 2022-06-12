cea8ef3c2f4c29e4e2193c92080a51f06594d7d1
Gonzalo_Sarsy <gonzalo.ibanez@sarsy-srl.com>
2022-06-12 20:11:45 -0300

    Se crean metodos test para sendgrid

--- SistemaLogin_Back/5.Transversal/Transversal.EmailService.Test/UnitTestEmailSend.cs
+++ SistemaLogin_Back/5.Transversal/Transversal.EmailService.Test/UnitTestEmailSend.cs
@@ -1,16 +1,31 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Transversal.EmailService.SendGrid;
 
 namespace Transversal.EmailService.Test
 {
     [TestClass]
     public class UnitTestEmailSend
     {
+        private EmailSendgridSender _emailSender;
+
+        [TestInitialize()]
+        public void Startup()
+        {
+            EmailSendGridConfiguration conf = new EmailSendGridConfiguration();
+            conf.From = "Gonza_28.00@hotmail.com";
+            conf.ApiKeyId = "nQtKzWYEQCOgc0mxrr-ylw";
+            conf.ApiKey = "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";
+            conf.DisplayName = "Gonzalo Ibañez";
+
+            _emailSender = new EmailSendgridSender(conf, new NullLogger<EmailSendgridSender>());
+        }
 
         [TestMethod]
         public async Task SendGridEmailAsync()
@@ -36,5 +51,17 @@ namespace Transversal.EmailService.Test
             }
         }
         
+        [TestMethod]
+        public async Task TestSendGrid()
+        {
+            string from = "Gonza_28.00@hotmail.com";
+            string subject = "Sending with SendGrid is Fun";
+            var to = new string[] { "gonzaloiba.28.00@gmail.com" };
+            string content = "test";
+            Message message = new Message(to,subject,content,from);
+
+            var result = await _emailSender.SendEmailAsync(message);
+        }
+
     }
 }
--- SistemaLogin_Back/5.Transversal/Transversal.EmailService/SendGrid/EmailSendgridSender.cs
+++ SistemaLogin_Back/5.Transversal/Transversal.EmailService/SendGrid/EmailSendgridSender.cs
@@ -1,10 +1,86 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Serilog;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Transversal.EmailService.SendGrid
 {
-    internal class EmailSendgridSender
+    public class EmailSendgridSender
     {
+        private readonly EmailSendGridConfiguration _emailConfig;
+        private readonly ILogger<EmailSendgridSender> _logger;
+        public EmailSendgridSender(EmailSendGridConfiguration emailConfig, ILogger<EmailSendgridSender> _logger)
+        {
+            _emailConfig = emailConfig;
+        }
+
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+
+            Send(emailMessage.Item1, emailMessage.Item2);
+        }
+
+        public async Task<string> SendEmailAsync(Message message)
+        {
+            var mailMessage = CreateEmailMessage(message);
+
+            return await SendAsync(mailMessage.Item1, mailMessage.Item2);
+        }
+
+        private (SendGridMessage, string) CreateEmailMessage(Message message)
+        {
+            var emailMessage = new SendGridMessage();
+            emailMessage.From = new EmailAddress(_emailConfig.From, _emailConfig.DisplayName);
+            emailMessage.AddTos(message.To.Select(t => new EmailAddress(t.Address)).ToList());
+            emailMessage.Subject = message.Subject;
+            emailMessage.HtmlContent = string.Format("<h2 style='color:red;'>{0}</h2>", message.Content);
+
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                byte[] fileBytes;
+                foreach (var attachment in message.Attachments)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        attachment.CopyTo(ms);
+                        fileBytes = ms.ToArray();
+                    }
+
+                    emailMessage.AddAttachment(attachment.FileName, Convert.ToBase64String(fileBytes));
+                }
+            }
+
+            string sendGridId = Guid.NewGuid().ToString();
+            emailMessage.AddCustomArg("message_id", sendGridId);
+            _logger.LogInformation("CreateEmailMessage Succeeded");
+            return (emailMessage, sendGridId);
+        }
+
+        private void Send(SendGridMessage mailMessage, string sendGridId)
+        {
+
+        }
+
+        private async Task<string> SendAsync(SendGridMessage mailMessage, string sendGridId)
+        {
+            var client = new SendGridClient(_emailConfig.ApiKey);
+            var response = await client.SendEmailAsync(mailMessage);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return sendGridId;
+            }
+
+            return null;
+        }
     }
 }
